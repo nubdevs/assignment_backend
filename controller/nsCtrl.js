19a1ef4888a4480788adbde48d16f9828fc2f943
Dev Kumar <nubdevs>
2023-06-09 20:49:03 +0530

    openai n imagine end point created

--- controller/nsCtrl.js
+++ controller/nsCtrl.js
@@ -1,43 +1,48 @@
 const Gas = require("../models/nsModel");
 
+const getAllData = async (req, res) => {
+  try {
+    const result = await Gas.find({});
+    res.status(200).send(result);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
 
-
-const getAllData=async(req,res)=>{
-    try{
-        const result = await Gas.find({})
-        res.status(200).send(result)
-    }catch(error){
-        res.status(500).send(error)
-    }
-}
-
-const filterData=async(req,res)=>{
+const filterData = async (req, res) => {
     try {
-        const result = await CallRecord
-          .where("callDateFrom")
-          .eq(req.body.query.callDateFrom)
-          .where("callDateTo")
-          .eq(req.body.query.callDateTo)
-          .where("phoneNumber")
-          .eq(req.body.query.phoneNumber)
-          .where("campaignId")
-          .eq(req.body.query.campaignId)
-          .where("agentId")
-          .eq(req.body.query.agentId)
-          .where("volunteerNumber")
-          .eq(req.body.query.volunteerNumber)
-    
-        res.status(200).send(result);
-      } catch (err) {
-        console.log(err);
-        if (res.status(500))
-          res.status(500).json({ error: true, message: "Internal Server Error" });
-        else if (res.status(404))
-          res.status(500).json({ error: true, message: "404 error ----" });
+      let query = Gas.find();
+  
+      if (req.body.topic) {
+        query = query.where("topic").eq(req.body.topic);
       }
-}
+  
+      if (req.body.sector) {
+        query = query.where("sector").eq(req.body.sector);
+      }
+  
+      if (req.body.country) {
+        query = query.where("country").eq(req.body.country);
+      }
+  
+      if (req.body.region) {
+        query = query.where("region").eq(req.body.region);
+      }
+  
+      if (req.body.min && req.body.max) {
+        query = query.where("intensity").gte(req.body.min).lte(req.body.max);
+      }
+  
+      const result = await query.exec();
+  
+      res.status(200).send(result);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ error: true, message: "Internal Server Error" });
+    }
+  };
 
 module.exports = {
-getAllData,
-filterData,
+  getAllData,
+  filterData,
 };
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const connectDB = require("./config/dbConnect");
 const cors = require("cors");
+const dotenv = require("dotenv")
 const nsRouter = require("./routes/nsRoute");
 const crRouter = require("./routes/callRoute")
+const { Configuration, OpenAIApi } = require('openai');
 
 
-require("dotenv").config();
+dotenv.config();
 
 const app = express();
 app.use(
@@ -17,14 +19,68 @@ app.use(
   })
 );
 app.use(cors());
-
+app.use(express.json())
+//------------------------conect db --------------------------------
 const client = connectDB();
-console.log(client, "ok");
+//-------------------openai config--------------------
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+// console.log(client, "ok");
 //-------------------------------------end Points-----------------------
 app.use("/ns", nsRouter);
-app.use("/cr",crRouter)
+app.use("/cr",crRouter);
+
+//------------------------open ai-----------------------------
+app.post('/openai', async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+    console.log(prompt)
+
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+
+      temperature: 0, // Higher values means the model will take more risks.
+      max_tokens: 2047, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      // top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+    });
+
+    res.status(200).send({
+      bot: response.data.choices[0].text
+    });
+
+  } catch (error) {
+    // console.error(error)
+    res.status(500).send(error || 'Something went wrong');
+  }
+})
 
 
+app.post("/imagine",async (req, res) => {
+  try {
+    const { prompt } = req.body;
+    console.log(prompt)
+
+
+    const aiResponse = await openai.createImage({
+      prompt,
+      n: 1,
+      size: '1024x1024',
+      response_format: 'b64_json',
+    });
+
+    const image = aiResponse.data.data[0].b64_json;
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    // console.error(error);
+    res.status(500).send(error?.response.data.error.message || 'Something went wrong');
+  }
+});
 
 // ---------------------------------------------------------------------
 
